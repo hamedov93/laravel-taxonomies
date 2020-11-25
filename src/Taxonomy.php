b1358a402c36d0fa8411ff1ec90d65de75458591
Mohamed Hamed <midking2013@gmail.com>
2020-11-25 22:15:41 +0200

    Fix media library error

--- src/Taxonomy.php
+++ src/Taxonomy.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Spatie\Translatable\HasTranslations;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\Image\Manipulations;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
